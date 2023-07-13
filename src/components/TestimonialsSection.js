1f62609559e5c972e4aea960ec533b6450f435fa
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2023-07-13 17:42:51 +0200

    Add testimonials section

--- src/components/TestimonialsSection.js
+++ src/components/TestimonialsSection.js
@@ -1,5 +1,34 @@
 const TestimonialsSection = () => {
-  return <section></section>;
+  return (
+    <section className="testimonials-section">
+      <div className="container">
+        <div className="testimonials">
+          <h1>Testimonials</h1>
+          <div className="testimonials-deck">
+            <div className="testimonial-card">
+              <span className="rating">★★★★★</span>
+              <div className="profile">
+                <div className="avatar">
+                  <img
+                    src={require('../images/footer_logo.png')}
+                    alt="Avatar"
+                    width={'48px'}
+                  />
+                </div>
+                <p>Bob</p>
+              </div>
+              <div className="comment">
+                <p>Amazing food!</p>
+              </div>
+            </div>
+            <div className="testimonial-card"></div>
+            <div className="testimonial-card"></div>
+            <div className="testimonial-card"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default TestimonialsSection;
