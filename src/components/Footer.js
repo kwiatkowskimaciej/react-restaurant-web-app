67e023fe5dcff23ff42319317d86b311f9e4714e
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2023-07-13 10:35:23 +0200

    Add the footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,35 @@
+import Nav from './Nav';
+
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <img
+        src={require('../images/footer_logo.png')}
+        alt="Little Lemon logo"
+        width={159}
+      />
+      <div>
+        <h3>Doormat navigation</h3>
+        <Nav />
+      </div>
+      <div>
+        <h3>Contact</h3>
+        <ul>
+          <li>Address</li>
+          <li>Phone number</li>
+          <li>Email</li>
+        </ul>
+      </div>
+      <div>
+        <h3>Social media links</h3>
+        <ul>
+          <li>Facebook</li>
+          <li>Instagram</li>
+          <li>Tik tok</li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
