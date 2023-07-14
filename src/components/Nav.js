ccea893d9de70e8921dfba8dab1d390823a142c4
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2023-07-14 17:09:51 +0200

    Add responsive design for small screens

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,28 +1,39 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Nav = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
   return (
     <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#menu">Menu</a>
-        </li>
-        <li>
-          <Link to="/reservations">Reservations</Link>
-        </li>
-        <li>
-          <a href="#order-online">Order Online</a>
-        </li>
-        <li>
-          <a href="#login">Login</a>
-        </li>
-      </ul>
+      <div className="menu-icon" onClick={handleShowNavbar}>
+        {showNavbar ? <span className="material-symbols-outlined">close</span> : <span className="material-symbols-outlined">menu</span>}
+      </div>
+      <div className={`nav-elements ${showNavbar && 'active'}`}>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#menu">Menu</a>
+          </li>
+          <li>
+            <Link to="/reservations">Reservations</Link>
+          </li>
+          <li>
+            <a href="#order-online">Order Online</a>
+          </li>
+          <li>
+            <a href="#login">Login</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
