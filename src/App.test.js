548e9c2a5d472fa2f5f34dbc4bf1f3f0d1563a22
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2023-07-16 20:10:30 +0200

    Add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import HomePage from './components/HomePage';
+import LandingSection from './components/LandingSection';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Router><LandingSection /></Router>);
+  const linkElement = screen.getByText('Little Lemon');
   expect(linkElement).toBeInTheDocument();
 });
