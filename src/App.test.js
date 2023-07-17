a825867289d5bc33901d511b885ed532b3b1e93e
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2023-07-17 17:15:21 +0200

    Add test for updateTimes

--- src/App.test.js
+++ src/App.test.js
@@ -1,27 +1,62 @@
-import { render, screen } from '@testing-library/react';
-import ReservationsPage, { initializeTimes, reducer } from './components/ReservationsPage';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import ReservationsPage from './components/ReservationsPage';
+import { act } from 'react-dom/test-utils';
 import { BrowserRouter } from 'react-router-dom';
+import { fetchAPI } from './reservationsAPI';
 
-test('Renders the ReservationPage heading', () => {
-  render(
-    <BrowserRouter>
-      <ReservationsPage />
-    </BrowserRouter>
-  );
-  const linkElement = screen.getByText('Reservations Page');
-  expect(linkElement).toBeInTheDocument();
+test('Renders the ReservationPage heading', async () => {
+  let component;
+  await act(async () => {
+    component = render(
+      <BrowserRouter>
+        <ReservationsPage />
+      </BrowserRouter>
+    );
+  });
+  const title = component.getByText('Reservations Page');
+  expect(title).toBeInTheDocument();
 });
 
-test('initializeTimes function returns the correct expected value', () => {
-  const expectedValue = '';
-  const result = initializeTimes();
-  expect(result).toEqual(expectedValue);
+test('initializeTimes function returns values', async () => {
+  let component;
+  await act(async () => {
+    component = render(
+      <BrowserRouter>
+        <ReservationsPage />
+      </BrowserRouter>
+    );
+  });
+
+  const loading = component.queryByText('Loading...');
+  expect(loading).not.toBeInTheDocument();
+
+  const selectElement = component.getByLabelText('Available times');
+  const options = selectElement.querySelectorAll('option');
+  expect(options.length).toBeGreaterThan(0);
 });
 
-test('updateTimes function returns the same value as state', () => {
-  const times = '18:00';
-  const initialState = '';
-  const action = { type: 'UPDATE_TIMES', times };
-  const result = reducer(initialState, action);
-  expect(result).toEqual(times);
-})
+test('updateTimes should return the same value provided in the state', async () => {
+  const selectedDate = '2023-07-28'
+  const wrongDate = '2023-07-31'
+
+  let component;
+  await act(async () => {
+    component = render(
+      <BrowserRouter>
+        <ReservationsPage />
+      </BrowserRouter>
+    );
+  });
+
+  await act(async () => {
+    const dateInput = component.getByLabelText('Choose date');
+    fireEvent.change(dateInput, { target: { value: selectedDate } });
+  });
+
+  const selectElement = component.getByLabelText('Available times');
+  const options = selectElement.querySelectorAll('option');
+  const values = Array.from(options).map(option => option.value);
+
+  expect(values).toEqual(fetchAPI(new Date(selectedDate)))
+  expect(values).not.toEqual(fetchAPI(new Date(wrongDate)))
+});
