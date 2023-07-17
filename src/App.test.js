21d0d66dcb61fb1a44524a8a682dd23a2ddf5ad0
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2023-07-17 17:17:26 +0200

    Edit test imports

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import ReservationsPage from './components/ReservationsPage';
 import { act } from 'react-dom/test-utils';
 import { BrowserRouter } from 'react-router-dom';
