e50249c65cd93b025eec67cc16aeec7952a653f9
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2023-07-23 20:11:48 +0200

    Add back to home by clicking logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,22 @@
+import { useNavigate } from 'react-router-dom';
 import Nav from './Nav';
 
 const Header = () => {
+  let navigate = useNavigate();
+  const handleClick = () => {
+    let path = '/';
+    navigate(path);
+  };
+
   return (
     <header>
       <img
         src={require('../images/logo.png')}
         alt="Little Lemon logo"
         width={200}
+        onClick={handleClick}
       />
-      <Nav className="header-nav"/>
+      <Nav className="header-nav" />
     </header>
   );
 };
